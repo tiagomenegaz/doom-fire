76c0e1da37739bbd9eba94a157f63d9cbb4c84db
Tiago Garcia <tiago.tiago22@gmail.com>
2019-02-26 00:20:41 -0300

    add canvas frame

--- fire.js
+++ fire.js
@@ -4,6 +4,7 @@ const fireHeight = 5;
 
 function start() {
   createFireDataStructure();
+  renderFire();
   console.log(firePixelArray);
 }
 
@@ -19,6 +20,24 @@ function calculateFirePropagation() {
 }
 
 function renderFire() {
+  let html = '<table cellpadding=0 cellspacing=0>';
+
+  for (let row = 0; row < fireHeight; row++) {
+    html += '<tr>';
+
+    for (let column = 0; column < fireWidth; column++) {
+      const pixelIndex = column + fireWidth * row;
+
+      html += '<td>';
+      html += pixelIndex;
+      html += '</td>';
+    }
+
+    html += '</tr>';
+  }
+
+  html += '</table>';
+  document.querySelector('#fireCanvas').innerHTML = html;
 }
 
 start();
